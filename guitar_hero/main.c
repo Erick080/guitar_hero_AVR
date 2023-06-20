c1bee2e1f33d76684e161a0552cb1d3803713600
erick <erickbranquinhomachado@gmail.com>
2023-06-19 21:43:39 -0300

    added timer configs

--- guitar_hero/main.c
+++ guitar_hero/main.c
@@ -15,18 +15,40 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdlib.h>
+#include <avr/interrupt.h>
 #include "nokia5110.h"
-#define PD0 red_led
-#define PD1 yellow_led
-#define PD2 green_led
+#define TIMER_CLK		F_CPU / 1024
+#define FREQ 10 // frequencias para setar timer q estora 10x por segundo
+int interrupt;
+int timer;
 
 uint8_t glyph[] = {0b00010000, 0b00100100, 0b11100000, 0b00100100, 0b00010000};
 
+ISR(TIMER1_COMPA_vect){ //a cada segundo incrementa timer
+    interrupt++;if(interrupt >= 10){interrupt = 0;timer++;}
+}
+
 int main(void)
 {
+    cli(); // desabilita interrupções
     DDRB &= ~((1 << PB1) | (1 << PB2) | (1 << PB3)); //seta pinos de entrada PB1,PB2 e PB3
-    DDRD |= (1<<red_led) | (1<<yellow_led) | (1<<green_led); //seta pinos de saida (leds)
+    PORTB |= (1 << PB0) | (1 << PB1)| (1 << PB2); //habilita pull-up
+    DDRD |= (1<<PD0) | (1<<PD1) | (1<<PD2); //seta pinos de saida (leds)
 
+    //-----configuracoes para habilitar interrupcoes-------
+    TCCR1A = 0;
+    TCCR1B = 0;
+    TCNT1  = 0;
+	OCR1A = (TIMER_CLK / FREQ) - 1; // seta o registrador output compare
+	TCCR1B |= (1 << WGM12); // liga modo CTC
+	TCCR1B |= (1 << CS12) | (1 << CS10);  // seta CS10 e CS12 para prescaler 1024
+	TIMSK1 |= (1 << OCIE1A); // habilita máscara do timer1
+    
+    //caso seja necessario adicionar um botao p/ interrupcao
+    /*EICRA = (1 << ISC01) | (1 << ISC00);  // interrupt sense control, borda de subida (rising edge) para INT0
+    EICRA |= (1 << ISC11) | (0 << ISC10); // interrupt sense control, borda de descida (falling edge) para INT1
+    EIMSK |= (1 << INT1) | (1 << INT0); */  // enable INT1 and INT0
+    //-------------------------------------
 
     nokia_lcd_init();
     nokia_lcd_clear();
@@ -36,8 +58,25 @@ int main(void)
     //nokia_lcd_write_string("Nice!\001", 2);
     nokia_lcd_drawcircle(5,5,5);
     nokia_lcd_render();
-     while(1);
+    PORTD  = 0b111;
+     while(1){
+        sei();
+        if(!(PINB & (1 << PB1))){
+            nokia_lcd_clear();
+            nokia_lcd_write_string("Botao apertado",1);
+            nokia_lcd_render();
+        } while(!(PINB & (1 << PB1))) _delay_ms(1);
+        if(timer >=10){
+            nokia_lcd_clear();
+            nokia_lcd_write_string("Acabou o tempo",1);
+            nokia_lcd_render();
+        }
+        
+     };
    
+
+
+
    /* while (1)
     {
         nokia_lcd_clear();
